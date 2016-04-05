d7bc460c9482562e2ef11c8317cb536da3c4895d
Hanbin Ma <qls6521@naver.com>
2016-04-05 20:30:18 +0900

    #10 handle user input(name)

--- app/src/main/java/kr/hs/emirim/qls6521/monchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/qls6521/monchicken/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mName = (EditText) findViewById(R.id.name);//find값으로 찾아주세요!, 타입이 안맞다! 타입캐스팅을 해줘야함
+        mName = (EditText)findViewById(R.id.name);//find값으로 찾아주세요!, 타입이 안맞다! 타입캐스팅을 해줘야함
         mButton = (Button) findViewById(R.id.btn_show_me_the_chicken);
         mButton.setOnClickListener(this);//익명클래스 널 클릭했을때 일어나는 일은 mainActivity에서 알아서 해줄거야!
 
@@ -31,7 +31,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     @Override
     protected void onResume() {
         super.onResume();
-        mName.setText("");
+        mName.setText(null);
     }
 
     /**
@@ -39,11 +39,36 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
      */
     @Override//애노테이션...?
     public void onClick(View v) {
-        Toast.makeText(this, "배고파요!", Toast.LENGTH_LONG).show();
-        Intent intent=new Intent(this, ResultActivity.class);
-        startActivity(intent);
+        String name = mName.getText().toString();
+//        if (name == null) {
+//            Toast.makeText(this, "이름을 입력해 주세요!", Toast.LENGTH_LONG).show();
+//        }
+//
+//        else {
+//            //Toast.makeText(this, "씨, 배고파요!", Toast.LENGTH_LONG).show();
+//            //toString은 오버라이딩해서 쓰려고하는 메소드이다 ->object 최상위 클래스에 있다
+//            Toast.makeText(this, name + "씨, 배고파요!", Toast.LENGTH_LONG).show();
+//            Intent intent = new Intent(this, ResultActivity.class);
+//            startActivity(intent);
+//        }
+
+        try{
+
+            Toast.makeText(this, name + "씨, 배고파요!", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this, ResultActivity.class);
+            startActivity(intent);
+
+        } catch(NullPointerException e){//NullPointerException이 실행되면 이 부분이 실행됨
+
+            Toast.makeText(this, "이름을 입력해 주세요!", Toast.LENGTH_LONG).show();
+
+        } catch(Exception e){//모든 예외의 어머니
+
+            Toast.makeText(this, "뭔지 모르지만 잘 안되네요!", Toast.LENGTH_LONG).show();
+
+        }
+
 
     }
-
 
 }
